33da5c307adac761f4eeb0974bef1b2d745c4784
Nate Sims <nsims@ggc.edu>
2022-08-12 00:39:33 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function () {
     let cartel = [{
         member: "Caleb Herron",
-        role: "Artistic Director, Percussion"
+        role: "President, Percussion"
     },
     {
         member: "Laura Gordy",
